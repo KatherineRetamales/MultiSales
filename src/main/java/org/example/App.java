c1f527ffea92accc4807517805dbe6c02ebadf0f
Toro Morales <javiera.toro.morales@accenture.com>
2024-01-09 18:03:08 -0300

    Se completa la clase pedido, usuarioDAO y app para la creacion de tablas

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,21 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.example.dao.UsuarioDAO;
+import org.example.models.Usuario;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
+
+    UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    Usuario nuevoUsuario = new Usuario("Priscila", "Sagua", "PA A 37", "Temuco", "priscila@email.com", "contrasena", "admin", 93000000);
+
+    usuarioDAO.insert(nuevoUsuario);
     }
+
 }
--- src/main/java/org/example/dao/UsuarioDAO.java
+++ src/main/java/org/example/dao/UsuarioDAO.java
@@ -1,7 +1,73 @@
 package org.example.dao;
 
 
+import org.example.models.Usuario;
+import org.example.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class UsuarioDAO {
+
+
+    public Usuario findById(Long id){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            return session.get(Usuario.class, 1l);
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public List<Usuario> findAll(){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            return session.createQuery("FROM Usuario").list();
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public List<Usuario> findByName(String name){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            return session.createQuery("FROM Usuario WHERE firstName='"+name+"'").list();
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public  void insert(Usuario Usuario){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(Usuario);
+            transaction.commit();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    public void update(Usuario Usuario){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.saveOrUpdate(Usuario);
+            transaction.commit();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    public void delete(Usuario Usuario){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.delete(Usuario);
+            transaction.commit();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
 
 }
